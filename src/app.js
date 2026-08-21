a898cef08f90d5ece2a8d92ec9a3b80a137e92fd
Ikrama Sattar <hafizikrma48@gmail.com>
2026-08-22 03:41:19 +0500

    add user routes for API version 1

--- src/app.js
+++ src/app.js
@@ -22,4 +22,10 @@ app.use(express.static('public'));
 
 app.use(cookieParser());
 
+//importing routes
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use('/api/v1/users', userRouter)
+
 export default app;
